874264980f5c4aba84049f008063a76dd3944f63
JadenPerleoni <jadenperleoni@gmail.com>
2022-07-07 10:00:03 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
+const { PrismaClient } = require("@prisma/client");
+
+const resolvers = {
+  Query: {
+    info: () => null,
+    feed: async (parents, args, context) => {
+      return context.prisma.link.findMany();
+    },
+  },
+  Mutation: {
+    post: (parents, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink;
+    },
+  },
+};
+
+const prisma = new PrismaClient();
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
+  context: {
+    prisma,
+  },
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));

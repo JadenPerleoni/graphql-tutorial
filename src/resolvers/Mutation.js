2d029420e954e2c74ddc50538470df8435e34688
JadenPerleoni <jadenperleoni@gmail.com>
2022-07-20 09:30:53 -0700

    fixed weird error with link ids

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,49 +1,6 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { APP_SECRET, getUserId } = require("../utils");
-
-async function signup(parent, args, context, info) {
-  // 1
-  const password = await bcrypt.hash(args.password, 10);
-
-  // 2
-  const user = await context.prisma.user.create({
-    data: { ...args, password },
-  });
-
-  // 3
-  const token = jwt.sign({ userId: user.id }, APP_SECRET);
-
-  // 4
-  return {
-    token,
-    user,
-  };
-}
-
-async function login(parent, args, context, info) {
-  // 1
-  const user = await context.prisma.user.findUnique({
-    where: { email: args.email },
-  });
-  if (!user) {
-    throw new Error("No such user found");
-  }
-
-  // 2
-  const valid = await bcrypt.compare(args.password, user.password);
-  if (!valid) {
-    throw new Error("Invalid password");
-  }
-
-  const token = jwt.sign({ userId: user.id }, APP_SECRET);
-
-  // 3
-  return {
-    token,
-    user,
-  };
-}
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET } = require('../utils');
 
 async function post(parent, args, context, info) {
   const { userId } = context;
@@ -52,46 +9,83 @@ async function post(parent, args, context, info) {
     data: {
       url: args.url,
       description: args.description,
-      postedBy: { connect: { id: userId } },
-    },
+      postedBy: { connect: { id: userId } }
+    }
   });
-  context.pubsub.publish("NEW_LINK", newLink);
+  context.pubsub.publish('NEW_LINK', newLink);
+
   return newLink;
 }
 
-async function vote(parent, args, context, info) {
-  // 1
-  const userId = context.userId;
+async function signup(parent, args, context, info) {
+  const password = await bcrypt.hash(args.password, 10);
+  const user = await context.prisma.user.create({
+    data: { ...args, password }
+  });
 
-  // 2
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user
+  };
+}
+
+async function login(parent, args, context, info) {
+  const user = await context.prisma.user.findUnique({
+    where: { email: args.email }
+  });
+  if (!user) {
+    throw new Error('No such user found');
+  }
+
+  const valid = await bcrypt.compare(
+    args.password,
+    user.password
+  );
+  if (!valid) {
+    throw new Error('Invalid password');
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user
+  };
+}
+
+async function vote(parent, args, context, info) {
+  const { userId } = context;
   const vote = await context.prisma.vote.findUnique({
     where: {
       linkId_userId: {
         linkId: Number(args.linkId),
-        userId: userId,
-      },
-    },
+        userId: userId
+      }
+    }
   });
 
   if (Boolean(vote)) {
-    throw new Error(`Already voted for link: ${args.linkId}`);
+    throw new Error(
+      `Already voted for link: ${args.linkId}`
+    );
   }
 
-  // 3
   const newVote = context.prisma.vote.create({
     data: {
       user: { connect: { id: userId } },
-      link: { connect: { id: Number(args.linkId) } },
-    },
+      link: { connect: { id: Number(args.linkId) } }
+    }
   });
-  context.pubsub.publish("NEW_VOTE", newVote);
+  context.pubsub.publish('NEW_VOTE', newVote);
 
   return newVote;
 }
 
 module.exports = {
+  post,
   signup,
   login,
-  post,
-  vote,
+  vote
 };
